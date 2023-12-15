2167e8575ccad55a1b73a4cf92a99cb1b3ed876a
XiaoWonton <130025970+XiaoWonton@users.noreply.github.com>
2023-12-15 03:53:32 -0500

    Create rolePlayingGame.java

---
+++ rolePlayingGame.java
@@ -0,0 +1,18 @@
+import java.util.Scanner
+
+class Main{
+  public static main void [] {
+    //get input from the user for the size of the grid
+    Scanner grid() = new Scanner(System.in);
+    private int numRows = grid.nextInt();
+    private int numCols = grid.nextInt();
+    private String[][] grid;
+    
+    //print out the grid
+    for (int i = 0; i < numRows; i++)
+      {
+        for (int j = 0; j < numCols; j++)
+          {
+            System.out.println();
+          }
+      }
