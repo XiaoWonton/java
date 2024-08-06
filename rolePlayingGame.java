053adbf9b3443ae6cfd6a45b5ad032dd5c32a2b2
Mikala Miller <130025970+XiaoWonton@users.noreply.github.com>
2024-08-06 03:05:30 -0400

    Update rolePlayingGame.java

--- rolePlayingGame.java
+++ rolePlayingGame.java
@@ -1,18 +1,35 @@
-import java.util.Scanner
+public class App {
 
-class Main{
-  public static main void [] {
-    //get input from the user for the size of the grid
-    Scanner grid() = new Scanner(System.in);
-    private int numRows = grid.nextInt();
-    private int numCols = grid.nextInt();
-    private String[][] grid;
-    
-    //print out the grid
-    for (int i = 0; i < numRows; i++)
-      {
-        for (int j = 0; j < numCols; j++)
-          {
-            System.out.println();
-          }
-      }
+    public static void main (String[] args){
+        Grid grid = new Grid(3, 3);
+
+        //String[][] grid = new String[3][3];
+
+        grid.place("X", 1, 1);
+        grid.display();
+
+        //Character mainChar = new Character("Mikala", 5, Character.ClassType.ASSASSIN);
+        //System.out.println("Character Name: " + mainChar.getName());
+        //System.out.println("Character Level: " + mainChar.getLevel());
+        //System.out.println("Character attack: " + mainChar.getAttack());
+
+        //Actor actor = new Actor(String name, int level, int hp, int attack, double critPercentage, double expPercentage);
+        //Actor opponent = new Actor(String name, int level);
+        //actor.getActor();
+
+        //Actor actor1 = new Actor();
+        //actor1.setName1("Mikala");
+        //System.out.println(actor1.getActor1());
+
+        Character protagonist = new Character("Mikala", 30, "KNIGHT");
+        Character imposter = new Character("Julia", 25, "ASSASSIN");
+        System.out.println(protagonist.getMainCharClass());
+        imposter.setExpPercentage(60);
+        protagonist.setExpPercentage(60);
+        System.out.println(imposter.getExpPercentage());
+        protagonist.attack(imposter);
+        }
+
+        
+        
+}
